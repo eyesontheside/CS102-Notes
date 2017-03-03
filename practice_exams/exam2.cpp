6763b772f32518de74231a2dafa75bcdd64395d8
Spencer Harper <hjw848@vols.utk.edu>
2017-03-03 15:50:54 -0500

    Added practice exam file

---
+++ practice_exams/exam2.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+#include <cmath>
+
+using namespace std;
+
+void MinMax(const double [], double&, double&, double&, double&);
+int GetRandom(int);
+
+const int MAX_VALUES = 30;
+
+int main() {
+
+    int seed;
+    int highest_value;
+    int divisor;
+    double minimum;
+    double maximum; 
+    double avg;
+    double stddev;         
+
+    while (true) {
+        //This is an infinite loop
+    }
+}
