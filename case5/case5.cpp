be53abcda01a03ce18ab57f74c4054e992a9aa81
Spencer Harper <hjw848@vols.utk.edu>
2017-04-01 11:50:41 -0400

    Added lower and search functions

--- case5/case5.cpp
+++ case5/case5.cpp
@@ -7,13 +7,19 @@
 
 using namespace std;
 
+string Lower(string);
+int SearchDistrict(string district_name, const vector<string> &);
+
+
 int main(int argc, char *argv[]) {
 
     ifstream fin;
+    istringstream sin;
+    string line, district_name;
+    double nonv_crime, v_crime, d_crime;
+    int num_lines=0, size;
     vector<string> district_names;
-    vector<int> nonviolent_crimes;
-    vector<int> violent_crimes;
-    vector<int> domestic_crimes;
+    vector<int> nonviolent_crimes, violent_crimes, domestic_crimes;
 
     // check to see if user entered the correct command line arguments
     if(argc != 3) {
@@ -21,13 +27,71 @@ int main(int argc, char *argv[]) {
         return -1;
     }
 
-    // open file
+    // convert command line argument into an integer
+    sin.str(argv[2]);
+    sin >> size;
+    sin.clear()
+
+    // open and check file 
     fin.open(argv[1]);
     if(fin.fail()) {
         cout << "file failed to open";
         return -1;
     }
 
+    // add data entries to vectors
+    while(getline(fin, line)) {
+
+        num_lines++;
+        sin.str(line);
+
+        sin >> district_name >> nonv_crime >> v_crime >> d_crime;
+
+        district_names.push_back(district_name);
+        nonviolent_crimes.push_back(nonv_crime * 10);
+        violent_crimes.push_back(v_crime * 10);
+        domestic_crimes.push_back(d_crime * 10);
+
+        sin.clear();
+
+    }
+
     fin.close();
+
+}
+
+
+// convert string to all lowercase
+string Lower(string str) {
+
+    int i, size;
+    size = str.length();
+
+    for(i=0; i<size; i++) {
+        str[i] = tolower(str[i]);
+    }
+
+    return str;
+
+}
+
+// search for index of district name while ignoring case
+int SearchDistrict(string district_name, const vector<string> & dist_vec) {
+
+    int i, index=-1, size;
+
+    size = dist_vec.size();
+    district_name = Lower(district_name);
+
+    for(i=0; i<size; i++) {
+
+        if(district_name == Lower(dist_vec[i])) {
+            index = i;
+            break;
+        }
+
+    }
+
+    return index;
 
 }
